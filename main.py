fe1987f63eb36d0bd233b26c462a98e604c20767
Paulolira <paulolira@sanesi.com>
2026-05-13 21:41:44 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+print('Setup Start')
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup End')
+
+
+print('Loop Start')
+while True:
+    #Check for all events -- checar todos os eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #Close window
+            quit()  # end pygame
+
+
