adffdba33f0aa0177a8e0722b7e5d8e14e95eaba
Paulolira <paulolira@sanesi.com>
2026-05-14 22:43:06 -0300

    initial commit asset

---
+++ code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.namr = None
+        self.enity_lista = None
+
+    def run(self, ):
+        pass
