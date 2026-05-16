751130089d24067fb94c0d3aab4d22f6d013bc53
Paulolira <paulolira@sanesi.com>
2026-05-16 20:59:36 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import background
+
+from code.Const import WIN_WIDTH
+from code.background import Background
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
 
-    def get_entily(self, entily_type):
-        pass
+    @staticmethod
+    def get_entity(entity_name: str, position=(0,0)):
+        match entity_name:
+            case'Level1Bg':
+                list_bg = []
+                for i in range (7):
+                    list_bg.append(Background(f'Level1Bg{i}', (0,0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+
+
+
--- code/level.py
+++ code/level.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
 from code.entity import Entity
+from code.entityFactory import EntityFactory
 
 
 class Level:
@@ -8,8 +11,15 @@ class Level:
         self.window = window
         self.name = name
         self.game_mode = game_mode
-        self.entity_lista: list[Entity] = []
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
 
-    def run(self, ):
+
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pygame.display.flip()
         pass
