19bca6e1b8719e8a9c3789d960ea8b9125da1617
Paulolira <paulolira@sanesi.com>
2026-05-21 23:45:03 -0300

    Creating  interations

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
+
 import background
 
 from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.background import Background
+from code.Enemy import Enemy
 from code.player import Player
 
 
@@ -22,4 +25,7 @@ class EntityFactory:
                 return Player('Player1',(10, WIN_HEIGHT/ 2 - 30))
             case 'Player2':
                 return Player('Player2', (10, WIN_HEIGHT / 2 + 30 ))
-
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT-40)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT-40)))
