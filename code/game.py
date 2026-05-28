13ded5542660a2b709ecc0e59ee0ab27f7e6cda9
Paulolira <paulolira@sanesi.com>
2026-05-27 23:10:57 -0300

    SQLITE3 IMPLEMENTATION FOR SCORE USING PROXY DESING PATTERN

--- code/game.py
+++ code/game.py
@@ -3,6 +3,7 @@
 import pygame
 
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Score import Score
 from code.level import Level
 from code.menu import Menu
 
@@ -17,6 +18,7 @@ class Game:
 
 
         while True:
+            score= Score(self.window)
             menu =  Menu(self.window)
             menu_return = menu.run()
 
@@ -27,6 +29,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close window
